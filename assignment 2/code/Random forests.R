d783f2ddfa2743698dd92064061b049426251889
francescopont <37176513+francescopont@users.noreply.github.com>
2020-01-05 23:40:51 +0100

    download random forests package

--- assignment 2/code/Random forests.R
+++ assignment 2/code/Random forests.R
@@ -30,13 +30,32 @@ random.forest.function <- function (training.corpus.dec, training.corpus.true, t
   #bigrams
   test.dtm.bigrams<- DocumentTermMatrix(test.dtm,list(dictionary=dimnames(training.dtm.bigrams)[[2]]))
   test.dtm.bigrams <- as.matrix(test.dtm.bigrams)
-  
   #test set with both unigrams and bigrams
   test.labels <- c(rep(0,80),rep(1,80))
   test.dtm <- cbind(test.dtm.unigrams, test.dtm.bigrams)
   #######################################################
+  #with only unigrams
+  training.dtm.unigrams <- as.data.frame(training.dtm.unigrams)
+  test.dtm.unigrams <- as.data.frame(test.dtm.unigrams)
+  OOB.matrix <- tuneRF(x =training.dtm.unigrams,
+                       y = training.labels,
+                       ntreeTry = 500, doBest = FALSE, plot = )
+  optimal.mtry <- OOB.matrix[which.min(OOB.matrix[,2]),1]
+  classifier <- randomForest(x = training.dtm[-318],
+                             y = training.dtm$label, ntree = 1000,
+                             mtry = optimal.mtry, type = "classification", err.rate = TRUE)
   
-  
+  error_rates <- classifier$err.rate[,1]
+  plot(classifier$err.rate[,1])
+  error_rates <- cbind(error_rates, c(1:1000))
+  optimal_ntree <- error_rates[which.min(error_rates[,1]), 2]
+  classifier <- randomForest(x = training.dtm[-318],
+                             y = training.dtm$label, mtry = optimal.mtry,
+                             ntree = optimal_ntree, type = "classification", err.rate = TRUE)
+  # Predicting the Test set results
+  predictions <- predict(classifier, newdata = test.dtm)
+  table(predictions,test.labels)
+  ############################################################
   training.dtm <- as.data.frame(training.dtm)
   test.dtm <- as.data.frame(test.dtm)
   training.dtm$label <- training.labels
